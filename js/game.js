3b2fc8f0b2ce3b5a6408ddd9ca6c45a5dc1969aa
kandersson135 <kandersson135@gmail.com>
2023-06-06 01:28:45 +0200

    Commented out dpad support

--- js/game.js
+++ js/game.js
@@ -273,7 +273,7 @@ document.addEventListener('keydown', (event) => {
   }
 });
 
-
+/*
 let timerValue = 0
 let interval;
 
@@ -331,6 +331,8 @@ actionButton.addEventListener('touchstart', () => {
   activeTeacher.useAbility();
 });
 
+*/
+
 /*
 // Key event listener
 const keysPressed = {};
@@ -384,8 +386,8 @@ startButton.addEventListener('click', () => {
     highScoresList.style.display = 'none';
     highScoreTitle.style.display = 'none';
     gameContainer.style.display = 'block';
-    gameControls.style.display = 'block';
-    actionButton.style.display = 'block';
+    //gameControls.style.display = 'block';
+    //actionButton.style.display = 'block';
     createCharacters(selectedTeacher.value);
     activeTeacher = characters[0];
     setInitialPositions();
@@ -484,8 +486,8 @@ function displayHighScores() {
 // Initialize the game
 function initGame() {
   teacherSelectScreen.style.display = 'block';
-  gameControls.style.display = 'none';
-  actionButton.style.display = 'none';
+  //gameControls.style.display = 'none';
+  //actionButton.style.display = 'none';
   displayHighScores();
 }
 
